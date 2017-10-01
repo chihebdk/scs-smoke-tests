af96d9406e8692fe9d4bd811b38c4e78c119caf2
Dkhil <Chiheb.Dkhil@emc.com>
2017-09-30 20:43:42 -0400

    first commit

---
+++ config-server-smoke-test/src/main/java/com/rbc/smoke_tests/config_server/Application.java
@@ -0,0 +1,29 @@
+package com.rbc.smoke_tests.config_server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+
+	@Value("${test.config.server:FAIL}")
+	public String status;
+	
+
+	@RequestMapping("/check")
+	public String check() {
+		return status;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+
+}
